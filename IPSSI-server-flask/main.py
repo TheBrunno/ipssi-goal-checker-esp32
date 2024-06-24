84008052fedc9ef420e438c1596f90b6f3350511
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2024-06-24 08:41:57 -0300

    image and new design

--- IPSSI-server-flask/main.py
+++ IPSSI-server-flask/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 socketio = SocketIO(app)
 
 @app.route('/')
